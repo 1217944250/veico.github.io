dbfe2fb4027d2689b1c33c6f8d2b99ccda1bae54
cine <1217944250@qq.com>
2022-10-14 15:42:09 +0800

    test

---
+++ cosmetics/model/category.js
@@ -0,0 +1,219 @@
+export function getCategoryList() {
+  return [
+    {
+      groupId: '24948',
+      name: '护肤品',
+      thumbnail:
+        'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+      children: [
+        {
+          groupId: '249481',
+          name: '护肤品',
+          thumbnail:
+            'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+          children: [
+            {
+              groupId: '249480',
+              name: '卸妆&清洁',
+              thumbnail:
+                'https://assets.sisley.com.cn//cms/upload/spu_images/837/6088cc55d729e.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '面膜&去角质',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/141565_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '眼唇护理',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/154050_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '化妆水类',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/103200_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '精华',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/150310_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '面霜',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/150050_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '修护',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/163001_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '美肤',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/160110_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '防晒',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/159410_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      groupId: '24948',
+      name: '彩妆类',
+      thumbnail:
+        'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+      children: [
+        {
+          groupId: '249481',
+          name: '彩妆类',
+          thumbnail:
+            'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+          children: [
+            {
+              groupId: '249480',
+              name: '粉底',
+              thumbnail:
+                'https://assets.sisley.com.cn//cms/upload/spu_images/644/5f51e38f3be88.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '遮瑕',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/161521_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '腮红&修容',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/182211_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '蜜粉&散粉',
+              thumbnail:
+                'https://assets.sisley.com.cn//cms/upload/spu_images/763/5fec3fc6b89bf.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '化妆刷',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/180010_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '睫毛膏',
+              thumbnail:
+                'https://assets.sisley.com.cn//cms/upload/spu_images/915/61a5b605c1ad6.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '眼影',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/187011_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '眼线笔',
+              thumbnail:
+                'https://assets.sisley.com.cn//cms/upload/spu_images/1055/62b1a1b6c0d84.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '唇釉',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/176001_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '唇膏',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/170341_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      groupId: '24948',
+      name: '香氛',
+      thumbnail:
+        'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+      children: [
+        {
+          groupId: '249481',
+          name: '香氛',
+          thumbnail:
+            'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+          children: [
+            {
+              groupId: '249480',
+              name: '女士香氛',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/198500_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '男士香氛',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/194150_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '身体乳',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/198570_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+            {
+              groupId: '249480',
+              name: '沐浴露',
+              thumbnail:
+                'https://assets.sisley.com.cn/upload/materials/pdp/headimgs/198560_01.jpg?x-oss-process=image/resize,w_900/quality,q_95',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      groupId: '24948',
+      name: '特殊类',
+      thumbnail:
+        'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+      children: [
+        {
+          groupId: '249481',
+          name: '特殊类',
+          thumbnail:
+            'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+          children: [
+            {
+              groupId: '249480',
+              name: '育发',
+              thumbnail:
+                'https://www.lorealparis.com.cn/-/media/project/loreal/brand-sites/oap/apac/cn/local-products/hair-care/packshots/6923700993059_packshot.png?w=200&hash=0C201869D832F21488A56A28EAB3C566412B8E9B',
+            },
+            {
+              groupId: '249480',
+              name: '染发',
+              thumbnail:
+                'https://www.lorealparis.com.cn/-/media/project/loreal/brand-sites/oap/apac/cn/local-products/hair-color/packshots/jgfh20201202.jpg?w=200&hash=F9D3FB4F24ECF537AD035BC4EEC3CA0363D8E366',
+            },
+            
+          ],
+        },
+      ],
+    },
+  ];
+}
